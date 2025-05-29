8689ae11403734dd4e76c2d697090c94a7038719
Torben Jost <torbenjost@proton.me>
2025-05-29 09:34:58 +0200

    cleaning up

--- frontend/src/components/Watchlist.tsx
+++ frontend/src/components/Watchlist.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect, useCallback} from "react";
+import {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import MovieDetailDeep from "./MovieDetailDeep.tsx";
